d80e77d1ae8d092db7959f25f63861f3191c65e6
Md. Abdul Halim Rafi <54027196+Md-Abdul-Halim-Rafi@users.noreply.github.com>
2020-10-30 21:25:17 +0600

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCYUWy85EYaycc_vooGr29VGu7C9GAYMzk",
-  authDomain: "cra-mui-async-seo.firebaseapp.com",
-  databaseURL: "https://cra-mui-async-seo.firebaseio.com",
-  projectId: "cra-mui-async-seo",
-  storageBucket: "cra-mui-async-seo.appspot.com",
-  messagingSenderId: "1049570714878",
-  appId: "1:1049570714878:web:f0defaefc983498fdef1af",
-  measurementId: "G-MEJ2JTTJFJ"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
